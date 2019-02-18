9d0fee3830d7c8daa9b752cb6b24adef08efb581
Kachawut <kachawut.sopa@gmail.com>
2019-02-18 10:52:15 +0700

    [API] Edit func UploadImage can crop image

--- main.go
+++ main.go
@@ -3,13 +3,19 @@ package main
 //! Golang(API) =>Echo + Mondgo Atlas
 
 import (
+	"bytes"
 	"crypto/tls"
+	"encoding/base64"
 	"fmt"
+	"image"
+	"image/jpeg"
+	"image/png"
 	"log"
 	"net"
 	"net/http"
 	"os"
 
+	"github.com/disintegration/imaging"
 	"github.com/globalsign/mgo"
 	"github.com/globalsign/mgo/bson"
 	"github.com/labstack/echo"
@@ -37,6 +43,11 @@ type Profile struct {
 }
 
 // ImageProfile is struct to update detail in data
+type ImageProfile struct {
+	ID       bson.ObjectId `json:"id" bson:"_id,omitempty"`
+	Username string        `json:"username" bson:"username,omitempty"`
+	Avatar   string        `json:"avatar" bson:"avatar,omitempty"`
+}
 
 const (
 	// mongoHost = "mongodb://127.0.0.1:27017"
@@ -149,26 +160,80 @@ func UploadImage(c echo.Context) (err error) {
 	dialInfo.DialServer = func(addr *mgo.ServerAddr) (net.Conn, error) {
 		return tls.Dial("tcp", addr.String(), tlsConfig)
 	}
-
 	session, err := mgo.DialWithInfo(dialInfo)
 	if err != nil {
 		fmt.Println("Can't connect database:", err)
 		return c.String(http.StatusInternalServerError, "Oh!, Can't connect database.")
 	}
 	defer session.Close()
-
 	var imgprofile Profile
 	err = c.Bind(&imgprofile) //* Receive data from Body(API)
 	if err != nil {
 		log.Println("Error: from c.Bind()")
 		return c.String(http.StatusInternalServerError, "Error: from c.Bind()")
 	}
-	err = session.DB(dbname).C(collection).Update(bson.M{"username": string(imgprofile.Username)}, bson.M{"$set": bson.M{"avatar": string(imgprofile.Avatar)}}) //* Choose database, collection and insert data
+	dataimg := imgprofile.Avatar
+	bs64tostr, _ := base64.StdEncoding.DecodeString(string(dataimg))
+	img, _, err := image.Decode(bytes.NewReader(bs64tostr))
+	if err != nil {
+		m := imaging.CropAnchor(img, 500, 500, imaging.Center)
+		buf := new(bytes.Buffer)
+		err = png.Encode(buf, m)
+		imageBit := buf.Bytes()
+		photoBase64 := base64.StdEncoding.EncodeToString(imageBit)
+		//fmt.Println(photoBase64)
+		if imgprofile.Username == "" { //* Forbid blank name and telno.
+			return &echo.HTTPError{Code: http.StatusBadRequest, Message: "invalid to or message fields"}
+		}
+		err = session.DB(dbname).C(collection).Update(bson.M{"username": string(imgprofile.Username)}, bson.M{"$set": bson.M{"avatar": string(photoBase64)}}) //* Choose database, collection and insert data
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "Error! <=from Update mongo.")
+		}
+		return c.JSON(http.StatusCreated, "Update successfully!")
+	}
+	m := imaging.CropAnchor(img, 500, 500, imaging.Center)
+	buf := new(bytes.Buffer)
+	err = jpeg.Encode(buf, m, nil)
+	imageBit := buf.Bytes()
+	photoBase64 := base64.StdEncoding.EncodeToString(imageBit)
+	//fmt.Println(photoBase64)
+	if imgprofile.Username == "" { //* Forbid blank name and telno.
+		return &echo.HTTPError{Code: http.StatusBadRequest, Message: "invalid to or message fields"}
+	}
+	err = session.DB(dbname).C(collection).Update(bson.M{"username": string(imgprofile.Username)}, bson.M{"$set": bson.M{"avatar": string(photoBase64)}}) //* Choose database, collection and insert data
 	if err != nil {
 		return c.String(http.StatusInternalServerError, "Error! <=from Update mongo.")
 	}
 	return c.JSON(http.StatusCreated, "Update successfully!") //* Done!
 }
+
+// func UploadImage(c echo.Context) (err error) {
+// 	tlsConfig := &tls.Config{}
+// 	dialInfo, err := mgo.ParseURL(mongoHost)
+
+// 	dialInfo.DialServer = func(addr *mgo.ServerAddr) (net.Conn, error) {
+// 		return tls.Dial("tcp", addr.String(), tlsConfig)
+// 	}
+
+// 	session, err := mgo.DialWithInfo(dialInfo)
+// 	if err != nil {
+// 		fmt.Println("Can't connect database:", err)
+// 		return c.String(http.StatusInternalServerError, "Oh!, Can't connect database.")
+// 	}
+// 	defer session.Close()
+
+// 	var imgprofile Profile
+// 	err = c.Bind(&imgprofile) //* Receive data from Body(API)
+// 	if err != nil {
+// 		log.Println("Error: from c.Bind()")
+// 		return c.String(http.StatusInternalServerError, "Error: from c.Bind()")
+// 	}
+// 	err = session.DB(dbname).C(collection).Update(bson.M{"username": string(imgprofile.Username)}, bson.M{"$set": bson.M{"avatar": string(imgprofile.Avatar)}}) //* Choose database, collection and insert data
+// 	if err != nil {
+// 		return c.String(http.StatusInternalServerError, "Error! <=from Update mongo.")
+// 	}
+// 	return c.JSON(http.StatusCreated, "Update successfully!") //* Done!
+// }
 
 // GetImage using for get image in mongo atlas
 func GetImage(c echo.Context) (err error) {
